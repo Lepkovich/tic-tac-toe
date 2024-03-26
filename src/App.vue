a2c9d89f4db1a2130e6da8bdf02c6a5ab71ab66a
Lepkovich <6332133@gmail.com>
2024-03-26 22:32:31 +0300

    31m of lesson. layout and user moves done

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
 <script setup>
-
+  import Board from './components/Board.vue'
+  import Score from './components/Score.vue'
 </script>
 
 <template>
-
+  <div class="title">
+    Крестики-нолики
+  </div>
+  <div class="game">
+    <Board />
+    <Score />
+  </div>
 </template>
 
 <style scoped>
-
+  .title {
+    font-size: 40px;
+    color: white;
+    text-align: center;
+    margin-top: 50px;
+  }
+  .game {
+    margin: 50px auto;
+    max-width: 324px;
+  }
 </style>
---
+++ src/components/Board.vue
@@ -0,0 +1,35 @@
+<script setup>
+  import Cell from './Cell.vue';
+  import {ref} from "vue";
+
+  const board = ref([
+    ['', 'o', ''],
+    ['', '', ''],
+    ['x', '', '']
+  ]);
+
+
+  function doMoveHandler(i, j, value) {
+    board.value[i][j] = value;
+  }
+
+</script>
+
+<template>
+    <div class="board">
+      <template v-for="(iValue, i) in board">
+        <template v-for="(jValue, j) in iValue">
+          <Cell :value="jValue" @do-move="(value) => doMoveHandler(i, j, value)"/>
+        </template>
+      </template>
+    </div>
+</template>
+
+<style scoped>
+.board {
+  display: grid;
+  grid-template-rows: repeat(3, 1fr);
+  grid-template-columns: repeat(3, 1fr);
+  border: 3px solid #d0d0d0;
+}
+</style>
